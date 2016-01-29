ecaefd93261309c2c09940b977801b6488bd4732
Fernando Paz <fernandopaz@softwareevolutivo.com.ec>
2016-01-29 18:44:22 -0500

    do punch

--- src/test/java/ec/com/se/HitTest.java
+++ src/test/java/ec/com/se/HitTest.java
@@ -2,11 +2,13 @@ package ec.com.se;
 
 import ec.com.se.business.Hit;
 import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created by pazfernando on 1/29/16.
  */
 public class HitTest {
+    @Test
     public void shouldReturnFivePoints() {
         Hit hit = new Hit();
         Assert.assertEquals("Player push puch", 5, hit.pushPunch());
